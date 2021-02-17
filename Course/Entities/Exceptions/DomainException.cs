9897e00b3d663d641a4b36324a95507db4e57bc2
Juarez <juarez.junior@outlook.com>
2021-02-17 08:52:55 -0300

    Initial commit

---
+++ Course/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course.Entities.Exceptions {
+
+    /* A Classe "DomainException" é uma classe personalizada
+     * que contém herança da classe "ApplicationException"
+     * que é uma classe do sistema para exceções personalizadas!
+    */
+    class DomainException : ApplicationException {
+
+        /* ": base(message)" informa que será usada a mensagem do sistema
+        ** como argumento também!
+        */
+        public DomainException(string message) : base(message) {
+        }
+    }
+}
